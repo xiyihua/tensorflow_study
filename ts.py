2f7f7ee98ec1de47e4ffd2074e06ae1764888c1b
xiyihua <xiyihua006@163.com>
2017-11-15 21:36:14 +0800

    *

--- ts.py
+++ ts.py
@@ -1,2 +1,6 @@
-import tensorflow
-print haha
+import tensorflow as tf
+a = tf.constant([1.0, 2.0], name='a')
+b = tf.constant([2.0, 3.0], name='b')
+result = a + b
+sess = tf.session()
+sess.run(result)
